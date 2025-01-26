b84121d091658e0ad2d01163f74b634010601290
Adrian Andres <xzoruuu@gmail.com>
2025-01-26 16:29:10 +0800

    progress

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+   content: ['./&.html'],
+   theme:{
+    extend: {
+        screens:{
+            "sm":"480px"
+        }
+    },
+    fontFamily:{
+        nunito:['Nunito', 'sans-serif']
+    }
+   },
+   plugins: [],
+}
